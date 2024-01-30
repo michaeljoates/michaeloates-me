80bf6e66286cc8d6a116b35d16a692a73f593ed9
Mike <m.j.oates@me.com>
2024-01-30 22:17:25 +1000

    added skills

---
+++ src/components/homePageComponents/Skills.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Skills() {
+    return (
+        <section class="skills-section">
+            <h2>Comprehensive Skills</h2>
+            <div class="skills-grid">
+                <div class="skill-item">
+                    <img src='"icon-url1' alt="Icon 1"/>
+                    <h3>Agile Development</h3>
+                    <p>Iterative and flexible software development</p>
+                </div>
+
+
+                <div class="skill-item">
+                    <img src='"icon-url1' alt="Icon 1"/>
+                    <h3>Languages</h3>
+                    <p>Iterative and flexible software development</p>
+                    
+                </div>
+
+                
+            </div>
+
+        </section>
+    );
+}
+
+export default Skills;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import Introduction from "../components/homePageComponents/Introduction.js";
+import Skills from "../components/homePageComponents/Skills.js";
   
   function Main() {
     return (
       <div>
         <Introduction />
+        <Skills />
       </div>
     );
   }
